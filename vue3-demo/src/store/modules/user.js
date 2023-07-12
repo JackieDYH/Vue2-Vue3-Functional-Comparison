3bc9447cf3891fab5b5051b236091413132233af
Jackie <1824561218@qq.com>
2023-07-12 14:32:12 +0800

    vuex使用属性

--- vue3-demo/src/store/modules/user.js
+++ vue3-demo/src/store/modules/user.js
@@ -1,30 +1,42 @@
+/*
+ * @Author: Jackie
+ * @Date: 2023-06-25 09:58:10
+ * @LastEditTime: 2023-07-12 14:30:43
+ * @LastEditors: Jackie
+ * @Description: file content
+ * @FilePath: /Vue2-Vue3-Functional-Comparison/vue3-demo/src/store/modules/user.js
+ * @version:
+ */
+const LOCAL_KEY_PREFIX = 'user_';
 export default {
-    namespaced: true,
-    state: {
-        isLogin: true,
-        name: 'dyh',
-        name2: 'hao',
-        num: 20,
+  namespaced: true,
+  state: {
+    isLogin: true,
+    name: 'dyh',
+    name2: 'hao',
+    num: 20
+  },
+  getters: {
+    getNum: (state) => {
+      return state.num;
     },
-    getters: {
-        getNum: (state) => {
-            return state.num;
-        },
-        getUserInfo: state => `${state.name}-${state.name2}`
+    getUserInfo: (state) => `${state.name}-${state.name2}`
+  },
+  mutations: {
+    setIsLogin(state, value) {
+      state.isLogin = value;
     },
-    mutations: {
-        setIsLogin(state, value) {
-            state.isLogin = value;
-        },
-        setNum(state, value) {
-            state.num += value;
-        }
-    },
-    actions: {
-        serNumSync({ commit },n) {
-            setTimeout(() => {
-                commit('setNum', n);
-            }, 1000);
-        },
-    },
+    setNum(state, value) {
+      state.num += value;
+    }
+  },
+  actions: {
+    serNumSync({ commit, dispatch, rootState }, n) {
+      // let num = rootState.user.num;
+      // dispatch('user/serNumSync');
+      setTimeout(() => {
+        commit('setNum', n);
+      }, 1000);
+    }
+  }
 };
