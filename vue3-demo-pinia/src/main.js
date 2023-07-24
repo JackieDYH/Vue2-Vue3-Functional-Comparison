17f71678a565f42ed07dd2db2dd5562d90b4da8f
Jackie <1824561218@qq.com>
2023-07-24 10:58:56 +0800

    init pinia

---
+++ vue3-demo-pinia/src/main.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: Jackie
+ * @Date: 2023-06-25 09:58:10
+ * @LastEditTime: 2023-07-24 10:58:02
+ * @LastEditors: Jackie
+ * @Description: pinia
+ * @FilePath: /vue3-demo-pinia/src/main.js
+ * @version:
+ */
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+createApp(App).use(router).mount('#app');
