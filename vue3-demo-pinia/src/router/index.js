ad15eeb6adbffef7b49d25a792452c1aa240fdf8
Jackie <1824561218@qq.com>
2023-08-07 19:54:01 +0800

    js文件使用pinia报错处理

--- vue3-demo-pinia/src/router/index.js
+++ vue3-demo-pinia/src/router/index.js
@@ -1,26 +1,42 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+/*
+ * @Author: Jackie
+ * @Date: 2023-06-25 09:58:10
+ * @LastEditTime: 2023-08-07 19:52:49
+ * @LastEditors: Jackie
+ * @Description: router
+ * @FilePath: /vue3-demo-pinia/src/router/index.js
+ * @version:
+ */
+import { createRouter, createWebHistory } from 'vue-router';
+// js文件使用报错处理
+import pinia from '@/store/store';
+import { useUserStore } from '@/store/user.js';
+const store = useUserStore(pinia);
+// 可以使用store
+console.log('router', store);
+
+import HomeView from '../views/HomeView.vue';
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: '/',
+    name: 'home',
+    component: HomeView
   },
   {
-    path: "/about",
-    name: "about",
+    path: '/about',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
+  routes
 });
 
 export default router;
---
+++ vue3-demo-pinia/src/store/store.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Jackie
+ * @Date: 2023-08-07 19:49:20
+ * @LastEditTime: 2023-08-07 19:50:23
+ * @LastEditors: Jackie
+ * @Description: pinia 处理js文件使用报错问题
+ * @FilePath: /vue3-demo-pinia/src/store/store.js
+ * @version:
+ */
+import { createPinia } from 'pinia';
+const pinia = createPinia();
+export default pinia;
