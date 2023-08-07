e27819df3590cede66ce2992dfabdd0461660dc9
Jackie <1824561218@qq.com>
2023-08-07 21:38:25 +0800

    统一配置pinia文件

--- vue3-demo-pinia/src/store/store.js
+++ vue3-demo-pinia/src/store/store.js
@@ -1,25 +1,26 @@
 /*
  * @Author: Jackie
  * @Date: 2023-08-07 19:49:20
- * @LastEditTime: 2023-08-07 20:30:22
+ * @LastEditTime: 2023-08-07 21:37:54
  * @LastEditors: Jackie
  * @Description: pinia 处理js文件使用报错问题
  * @FilePath: /vue3-demo-pinia/src/store/store.js
  * @version:
  */
 import { createPinia } from 'pinia';
+// 数据持久化
+import piniaPluginPersist from 'pinia-plugin-persist';
+
 const pinia = createPinia();
 
-// // 为了解决setup语法下不支持 .$reset() 方法的问题
-// pinia.use(({ store }) => {
-//   const initialState = JSON.parse(JSON.stringify(store.$state));
-//   store.$reset = () => {
-//     store.$state = JSON.parse(JSON.stringify(initialState));
-//   };
-// });
+pinia.use(piniaPluginPersist);
 
-// // 数据持久化
-// import piniaPluginPersist from 'pinia-plugin-persist';
-// pinia.use(piniaPluginPersist);
+// 为了解决setup语法下不支持 .$reset() 方法的问题
+pinia.use(({ store }) => {
+  const initialState = JSON.parse(JSON.stringify(store.$state));
+  store.$reset = () => {
+    store.$state = JSON.parse(JSON.stringify(initialState));
+  };
+});
 
 export default pinia;
